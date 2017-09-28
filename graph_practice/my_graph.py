6268060d6a974cccedeaea4912515406e3cd5a4e
Arti Intelli <31372701+cs4660csula@users.noreply.github.com>
2017-09-28 13:19:42 -0700

    Update my_graph.py
    Adding cities and roads with distances.

--- graph_practice/my_graph.py
+++ graph_practice/my_graph.py
@@ -28,3 +28,38 @@ def getNames(self):
 
 def getWeight(self):
     return self._distance
+
+  
+  
+  
+  """ Adding city code for the graph
+  The following is how we might set up our node list and edge list for the 
+city example. We’ll create our five cities, each with some population, and 
+add  those  to  the  city  list.  For  example,  we  create  a  node  for  Rivertown  
+with a population of 100 and append it to the cities list.
+  
+  """
+  
+  
+cities = []
+roads = []
+city = node('Rivertown', 1000)
+cities.append(city)
+city = node('Brookside', 1500)
+cities.append(city)
+city = node('Hillsview', 500)
+cities.append(city)
+city = node('Forrest City', 800)
+cities.append(city)
+city = node('Lakeside', 1100)
+cities.append(city)
+road = edge('Rivertown', 'Brookside', 100)
+roads.append(road)
+road = edge('Rivertown', 'Hillsview', 50)
+roads.append(road)
+road = edge('Hillsview', 'Brookside', 130)
+roads.append(road)
+road = edge('Hillsview', 'Forrest City', 40)
+roads.append(road)
+road = edge('Forrest City', 'Lakeside', 80)
+roads.append(road)
