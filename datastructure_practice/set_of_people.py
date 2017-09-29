ddadb2246554e101b251aa713579ce1ed8cc39f0
Arti Intelli <31372701+cs4660csula@users.noreply.github.com>
2017-09-28 17:07:46 -0700

    Update set_of_people.py

--- datastructure_practice/set_of_people.py
+++ datastructure_practice/set_of_people.py
@@ -30,3 +30,28 @@ people = set(['John', 'Sue', 'Bill'])
 
 OUTPUT:
 Yes!
+
+
+##########################
+"""
+Sets have some additional operations defined on them that can be very
+useful. First, sets have an “add” method defined. To add a new element to
+a set, just call “add” as a method on that set and pass in the new element
+as a parameter. In the following example, we have a set of friends from
+work, and we add “Kathy” to that list. You can see from the output that
+Kathy is added to the set. Likewise, there is a “remove” method defined.
+In the example, we remove “Fred” from the list.
+"""
+
+work_friends = {'Sue', 'Eric', 'Fred'}
+        print(work_friends)
+work_friends.add('Kathy')
+        print(work_friends)
+work_friends.remove('Fred')
+        print(work_friends)
+
+
+OUTPUT:
+{'Fred', 'Eric', 'Sue'}
+{'Fred', 'Eric', 'Sue', 'Kathy'}
+{'Eric', 'Sue', 'Kathy'}
