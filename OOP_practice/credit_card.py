0b7063e8c68d28489797dfcd1ec7343a7b3a8203
Arti Intelli <31372701+cs4660csula@users.noreply.github.com>
2017-09-28 17:19:08 -0700

    Update credit_card.py

--- OOP_practice/credit_card.py
+++ OOP_practice/credit_card.py
@@ -52,3 +52,31 @@ return True
 def make payment(self, amount):
 ”””Process customer payment that reduces balance.”””
 self. balance −= amount
+
+
+
+##############################Test
+if name == __main__ :
+wallet = [ ]
+wallet.append(CreditCard( John Bowman , California Savings ,
+5391 0375 9387 5309 , 2500) )
+wallet.append(CreditCard( John Bowman , California Federal ,
+3485 0399 3395 1954 , 3500) )
+wallet.append(CreditCard( John Bowman , California Finance ,
+5391 0375 9387 5309 , 5000) )
+
+for val in range(1, 17):
+wallet[0].charge(val)
+wallet[1].charge(2 val)
+wallet[2].charge(3 val)
+
+for c in range(3):
+print( Customer = , wallet[c].get customer( ))
+print( Bank = , wallet[c].get bank())
+print( Account = , wallet[c].get account( ))
+print( Limit = , wallet[c].get limit( ))
+print( Balance = , wallet[c].get balance())
+while wallet[c].get balance( ) > 100:
+wallet[c].make payment(100)
+print( New balance = , wallet[c].get balance())
+print( )
