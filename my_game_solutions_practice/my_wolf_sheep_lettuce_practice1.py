96f53765f32c785a50fee2668c2569c36d4713d3
Arti Intelli <31372701+cs4660csula@users.noreply.github.com>
2017-09-30 12:37:04 -0700

    Update my_wolf_sheep_lettuce_practice1.py

--- my_game_solutions_practice/my_wolf_sheep_lettuce_practice1.py
+++ my_game_solutions_practice/my_wolf_sheep_lettuce_practice1.py
@@ -4,3 +4,21 @@ My solution to the game is as follows
 
 """
 ######################################
+
+We have three variables in the form of 
+
+
+String wolf, goat, lettuce;
+
+
+array[] boatArray = []; 
+
+
+piece_of_land_A = A
+
+
+piece_of_land_B = B
+
+
+We need to move all three Strings from A to B.
+However, we can't lost any elements in the process.
