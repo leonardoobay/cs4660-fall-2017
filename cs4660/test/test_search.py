9fbe8fd94b41ca0352816fd6625f261b031e7bb1
cs4660csula <cs4660.usa@gmail.com>
2017-10-07 11:44:31 -0700

    last few remaining typos and errors gettig fixed

--- cs4660/test/test_search.py
+++ cs4660/test/test_search.py
@@ -268,13 +268,13 @@ class TestAStar(unittest.TestCase):
         )
         self.assertTrue(string_equal_without_order(path, expected_path))
         print("A_star_4: %.6f seconds" % (time.time() - start_time))
-
+    # this method below had a typo
     def test_a_star_5(self):
         start_time = time.time()
         expected_path = "SSSSSSSSSSEESSEESESESSEESSEESESESESESSEESESESESESESSESEESESESSESEESSEESSEESESESESESSESEESSESESEESSESEESESSESEESESESESESSEESESESESESESESESESSEESESESESESESSEESSEESESSESEESSEESESSEESESESESESESSEESESESSEESESSESEESSEESESESESSEESSESEESESSESESESESEESSEESESESESESESESESESESESESESESESSEESESSEESSEESESESESSEESESESSEESESESSEESESESESESESESESESESESESESSESEESSEESESSEESESSEESSEESESSEESESESESESESESESESESSEESEEEESSSSSE"
         path = utils.convert_edge_to_grid_actions(
             searches.a_star_search(
-                self.graph_4,
+                self.graph_5, # <-fixed this typo
                 graph.Node(utils.Tile(4, 0, "@1")),
                 graph.Node(utils.Tile(201, 206, "@5"))
             )
